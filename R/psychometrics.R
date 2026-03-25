05d04f432fc95a4f36f0d979093fbceca0f977a8
giorgio.arcara <giorgio.arcara@gmail.com>
2026-03-25 06:38:54 +0100

    added example to simulate obs and to manual

--- R/psychometrics.R
+++ R/psychometrics.R
@@ -32,9 +32,9 @@ return(df)
 #' A function to simulate scores providing reliability
 #' 
 #' @param 
-#' n.obs = number of observations
+#' n = number of observations
 #' Ts = the current True score (on which observed are computed)
-#' Rel = reliability value (from 0 to 1)
+#' rel = reliability value (from 0 to 1)
 #' mu_true = mean of population
 #' sd_true = standard deviation of the population
 #' label = the title of the plot
@@ -42,8 +42,10 @@ return(df)
 #'
 #' @returns a plot with the simulated scores.
 #' @export
+#' @examples
+#' simulate_obs(Ts = 5, n = 20, rel=0.8, mu_true = 8, sd_true = 2)
 
-simulate_obs <- function(Ts, n.obs, Rel = 0.7, mu_true, sd_true, label="True - observed relationship", plot_thresh=FALSE){
+simulate_obs <- function(Ts, n, rel = 0.7, mu_true, sd_true, label="True - observed relationship", plot_thresh=FALSE){
   
   sd_error <- sd_true * sqrt((1 - rel) / rel)
   Xs = Ts + rnorm(n, mean = 0,    sd = sd_error)
@@ -69,20 +71,21 @@ simulate_obs <- function(Ts, n.obs, Rel = 0.7, mu_true, sd_true, label="True - o
   }
   
   # plot observed scores as compared to True Score
-  plot(1:n.obs, Xs, ylim=ylims, pch=21, bg=curr_bg, main=label)
-  lines(1:n.obs, Xs)
+  plot(1:n, Xs, ylim=ylims, pch=21, bg=curr_bg, main=label)
+  lines(1:n, Xs)
   abline(h=Ts)
   if (plot_thresh){
     abline(h=cut_off, lty=2) 
   }
   
-  png(paste0("Figures/Figure_true_obs_", sd_true, ".png"), res=200, height=800, width=1200)
-  plot(1:n.obs, Xs, ylim=ylims, pch=21, bg=curr_bg, main=label)
-  lines(1:n.obs, Xs)
-  abline(h=Ts)
-  if (plot_thresh){
-    abline(h=cut_off, lty=2) 
-  }
-  dev.off()
+  #png(paste0("Figure_true_obs_", sd_true, ".png"), res=200, height=800, width=1200)
+  #plot(1:n, Xs, ylim=ylims, pch=21, bg=curr_bg, main=label)
+  #lines(1:n, Xs)
+  #abline(h=Ts)
+  #if (plot_thresh){
+  #  abline(h=cut_off, lty=2) 
+  #}
+  #dev.off()
   
 }
+
