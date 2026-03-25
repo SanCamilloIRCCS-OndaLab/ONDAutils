83807ad2f6dc5ecabe584b4d3c117f18b09bdea6
giorgio.arcara <giorgio.arcara@gmail.com>
2026-03-25 06:28:32 +0100

    added pacakge help page

---
+++ R/ONDAutils-package.R
@@ -0,0 +1,18 @@
+#' @title ONDA Utilities
+#'
+#' @description
+#' A collection of utility functions for the ONDA workflow.
+#'
+#' @details
+#' \code{ONDAutils} provides a set of general-purpose utility functions
+#' designed to support and streamline ONDA-related data processing tasks.
+#'
+#' The package is organized around helper functions that cover common
+#' operations needed across ONDA projects.
+#'
+#' @author Your Name \email{you@@example.com}
+#'
+#' @docType package
+#' @name ONDAutils
+#' @keywords internal
+"_PACKAGE"
